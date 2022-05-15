d25b88ba7c3f85e943a44f6fbd6ab1790979575d
unknown <khaledjamal5946@gmail.com>
2022-05-15 09:39:08 +0430

    25-02 first

--- FirstAppWithApi/app/Http/Livewire/Client/ListSession.php
+++ FirstAppWithApi/app/Http/Livewire/Client/ListSession.php
@@ -13,6 +13,7 @@ class ListSession extends Component
 {
     use WithPagination;
     public $search="";
+    public $descAsc=false;
 
     public function render()
     {
@@ -24,5 +25,49 @@ class ListSession extends Component
     public function mount()
     {
         $this->u_id = Auth::user()->id;
+    }
+    public function getAllSession()
+    {
+        $this->allsessions = Sessions::where([
+            ['u_id',$this->u_id],
+            ['is_ended',0],
+            ['name', 'like', '%'.$this->search.'%']
+        ])->get();
+    }
+    public function editeSession($sid)
+    {
+        
+    }
+    public function getSessionsByAscOrDesc($flag)
+    {
+        if($flag=="asc"){
+            $this->descAsc =True;
+        }else{
+            $this->descAsc =False;
+        }
+        $this->allsessions = Sessions::where([
+            ['u_id',$this->u_id],
+            ['is_ended',0],
+            ['name', 'like', '%'.$this->search.'%']
+        ])->orderBy('start_date',$flag)->get();
+    }
+    public function deleteSession($sid)
+    {
+        try{
+            Sessions::find($sid)->delete();
+            
+            $this->getAllSession();
+            
+            $this->dispatchBrowserEvent('alert',[
+                'type'=>'success',
+                'message'=>"جلسه با موفقیت حذف شد!!"
+            ]);
+        }catch(\Exception $e){
+            $this->dispatchBrowserEvent('alert',[
+                'type'=>'error',
+                'message'=>"مشکلی پیش آمده لطفا دوباره امتحان کنید!!"
+            ]);
+        
+        }
     }
 }
