f8f56a58132c8b1d965d05c5e0ef4144807b20f9
KhjMeister <95680946+KhjMeister@users.noreply.github.com>
2022-06-09 14:52:29 +0430

    Update api.php

--- FirstAppWithApi/routes/api.php
+++ FirstAppWithApi/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\CORS;
 
 use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\CategoryController;
@@ -13,10 +14,10 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('/login',       [AuthController::class, 'login']);
-    Route::post('/register',    [AuthController::class, 'register']);
-    Route::post('/logout',      [AuthController::class, 'logout']);
-    Route::post('/refresh',     [AuthController::class, 'refresh']);
+    Route::post('/login',       [AuthController::class, 'login'])->middleware(CORS::class);
+    Route::post('/register',    [AuthController::class, 'register'])->middleware(CORS::class);
+    Route::post('/logout',      [AuthController::class, 'logout'])->middleware(CORS::class);
+    Route::post('/refresh',     [AuthController::class, 'refresh'])->middleware(CORS::class);
 
     Route::get('/user-profile', [ProfileController::class, 'userProfile']);    
     Route::put('/edite-profile/{id}',[ProfileController::class, 'updateProfile']);
